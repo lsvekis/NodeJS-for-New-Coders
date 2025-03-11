992cfadd981273b2ca15204a1ca3cfcae7cb5f20
Laurence Svekis <lassvekis@gmail.com>
2025-03-11 17:14:11 -0400

    Create chatServer.js

---
+++ chatServer.js
@@ -0,0 +1,19 @@
+// chatServer.js
+const WebSocket = require('ws');
+const server = new WebSocket.Server({ port: 8080 });
+server.on('connection', (socket) => {
+  console.log('A new client connected.');
+  // Send a welcome message to the client
+  socket.send('Welcome to the chatroom!');
+  // Listen for incoming messages from clients
+  socket.on('message', (message) => {
+    console.log(`Received: ${message}`);
+    // Broadcast the message to all connected clients
+    server.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+});
+console.log('Chat server is running on ws://localhost:8080');
